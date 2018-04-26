ddd24024ab4de44e46a21fefcb13e25b353e9395
TestDp <Cristianmg13@hotmail.com>
2018-04-26 10:06:24 -0500

    se agrega el buscador por nombre a los usuarios registrados y asistentes

--- src/pages/asistentes-activos/asistentes-activos.ts
+++ src/pages/asistentes-activos/asistentes-activos.ts
@@ -15,33 +15,58 @@ import { EcoticketsServiceProvider } from "../../providers/ecotickets-service/ec
   templateUrl: 'asistentes-activos.html',
 })
 export class AsistentesActivosPage {
-Evento;
-ListaAsistentes;
-  constructor(public navCtrl: NavController, 
+  Evento;
+  ListaAsistentes;
+  ListaAsistentesFiltro;
+  constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public ecoticketsService: EcoticketsServiceProvider,
     public cargando: LoadingController) {
-      this.Evento = navParams.get("Evento");
+    this.Evento = navParams.get("Evento");
   }
 
   ionViewDidLoad() {
     let loaderAsistentes = this.cargando.create({
       content: 'Cargando Asistentes...',
-    });   
+    });
 
     loaderAsistentes.present().then(() => {
       this.ecoticketsService.AsistentesActivos(this.Evento.id).subscribe(
         (data: any) => { // Success
-          this.ListaAsistentes=JSON.parse(data._body);
-         
+          this.ListaAsistentes = JSON.parse(data._body);
+          this.ListaAsistentesFiltro = JSON.parse(data._body);
           loaderAsistentes.dismiss();
         },
         (error) => {
           console.error(error);
           loaderAsistentes.dismiss();
         }
-      );      
+      );
     });
+  }
+
+  getUsuariosRegistrados(ev) {
+    this.ListaAsistentes.usuariosRegistrados = this.ListaAsistentesFiltro.usuariosRegistrados;
+    // se obtiene el valor ingresado
+    var val = ev.target.value;
+    //si el valor es vacio no se hace el filtro
+    if (val && val.trim() != '') {
+      this.ListaAsistentes.usuariosRegistrados = this.ListaAsistentes.usuariosRegistrados.filter((item) => {
+        return (item.Nombres.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  getAsistentes(ev) {
+    this.ListaAsistentes.Asistentes = this.ListaAsistentesFiltro.Asistentes;
+    // se obtiene el valor ingresado
+    var val = ev.target.value;
+    //si el valor es vacio no se hace el filtro
+    if (val && val.trim() != '') {
+      this.ListaAsistentes.Asistentes = this.ListaAsistentes.Asistentes.filter((item) => {
+        return (item.Nombres.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
